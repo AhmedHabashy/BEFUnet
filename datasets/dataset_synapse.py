137d49be05d00b43f7e638a69197d27c551302f7
Ahmed Habashy <122102338+AhmedHabashy@users.noreply.github.com>
2024-05-19 12:08:12 +0300

    Update dataset_synapse.py
    change npz to h5 to adapt the code to brats2020

--- datasets/dataset_synapse.py
+++ datasets/dataset_synapse.py
@@ -59,9 +59,9 @@ class Synapse_dataset(Dataset):
     def __getitem__(self, idx):
         if self.split == "train":
             slice_name = self.sample_list[idx].strip('\n')
-            data_path = os.path.join(self.data_dir, slice_name+'.npz')
-            data = np.load(data_path)
-            image, label = data['image'], data['label']
+            data_path = os.path.join(self.data_dir, slice_name+'.h5')
+            data = h5py.File(data_path)
+            image, label = data['image'][:], data['label'][:]
         else:
             vol_name = self.sample_list[idx].strip('\n')
             filepath = self.data_dir + "/{}.npy.h5".format(vol_name)
